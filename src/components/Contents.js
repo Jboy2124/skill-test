5fe28437e089e93a28fc5e7d38f968308f448df5
Jboy Abecia <jufel.abecia24@gmail.com>
2022-10-29 17:03:37 +0800

    adding scroll down

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,34 +1,52 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 import Cards from '../utils/Cards'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Contents = () => {
     const [data, setData] = useState([]);
-
+    const [pages, setPages] = useState(0);
+    const API_URL = 'https://api.spacexdata.com/v4/launches/';
     const fetchData = async() => {
-        await axios.get('https://api.spacexdata.com/v4/launches/')
+        await axios.get(`${API_URL}?&maxResults=10`)
                     .then(response => {
                         setData(response.data);
-                    })
+                        setPages(response.data.length);
+                    })       
     }
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    const getMoreData = () => {
+        // setTimeout(() => {
+        //     fetchData();
+        // }, 1000);
+    }
+
+    const hasMoreData = () => {
+
+    }
+
   return (
     <div className='container flex flex-col mx-auto min-h-screen bg-white border border-gray-400 rounded px-2 pt-5'>
         <div>
-            {
-                data.map(items => {
-                    return(
-                        <Cards image={items.links.patch.small} 
-                               flightNumber={items.flight_number} 
-                               missionName={items.name}
-                               details={items.details} />
-                    )
-                })
-            }
+            <InfiniteScroll dataLength={data.length - 20}
+                            next={getMoreData}
+                            hasMore={hasMoreData}>
+                {
+                    data.map(items => {
+                        return(
+                            <Cards image={items.links.patch.small} 
+                                flightNumber={items.flight_number} 
+                                missionName={items.name}
+                                yearLaunch={items.date_utc}
+                                details={items.details} />
+                        )
+                    })
+                }
+            </InfiniteScroll>
         </div>
     </div>
   )
--- src/utils/Cards.js
+++ src/utils/Cards.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Cards = (props) => {
-    const { image, flightNumber, missionName, details } = props;
+    const { image, flightNumber, missionName, yearLaunch, details } = props;
+    const extractedYear = new Date(yearLaunch).getFullYear();
   return (
     <div className='flex flex-row items-center h-40 px-5 space-x-5'>
         <div className='p-5 bg-gray-200 rounded w-1/12'>
@@ -9,7 +10,7 @@ const Cards = (props) => {
         </div>
         <div className='flex flex-col space-y-1 w-11/12'>
             <div className='flex items-center'>
-                <p className='text-2xl'>{flightNumber}: {missionName} (2010)</p>
+                <p className='text-2xl'>{flightNumber}: {missionName} ({extractedYear})</p>
             </div>
             <div>
                 <p className='text-sm text-gray-400'>{details}</p>
